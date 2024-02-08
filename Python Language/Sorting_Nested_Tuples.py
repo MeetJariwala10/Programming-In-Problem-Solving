bce4c3e5d787cf23594b2b058d668c030576463b
Meet Jariwala <134535589+MeetJariwala10@users.noreply.github.com>
2024-02-08 23:58:46 +0530

    Update and rename python71.py to Sorting_Nested_Tuples.py

--- Python Language/Sorting_Nested_Tuples.py
+++ Python Language/Sorting_Nested_Tuples.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[19]:
-
-
 # Q-71 ---> A python program to sort a tuple with nested tuples
 
 #t1 = ((1, 'meet', 'maths', 100),(2, 'harsh', 'chemistry', 90), (3, 'hello', 'ES', 10), (4, 'Hi', 'egd', 99))
@@ -13,22 +7,4 @@ print(t1)
 
 sort_key = int(input("Enter key :"))
 sorted(t1, key = lambda x : x[sort_key])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
